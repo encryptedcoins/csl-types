1ad2f5f55818b3cd502482415a62b40c8b846870
encoins <111982001+encryptedcoins@users.noreply.github.com>
2023-01-10 17:11:02 +0100

    Add minimal transaction inputs and outputs support

---
+++ src/CSL.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module CSL where
+
+import           Data.Aeson          (ToJSON, FromJSON)
+import qualified Data.Map            as Map
+import           Data.Text           (Text)
+import           GHC.Generics        (Generic)
+import           PlutusTx.Prelude    (BuiltinByteString)
+import           Prelude
+
+data TransactionInput = TransactionInput {
+        transaction_id :: BuiltinByteString,
+        index          :: Integer
+    }
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving (ToJSON, FromJSON)
+
+type TransactionInputs = [TransactionInput]
+
+type MultiAsset = Map.Map BuiltinByteString (Map.Map BuiltinByteString Text)
+
+data TransactionOutput = TransactionOutput
+    {
+        address     :: BuiltinByteString,
+        amount      :: Integer,
+        multiasset  :: MultiAsset,
+        plutus_data :: Maybe (),
+        script_ref  :: Maybe ()
+    }
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving (ToJSON, FromJSON)
+
+type TransactionOutputs = [TransactionOutput]
+
+data TransactionUnspentOutput = TransactionUnspentOutput
+    {
+        input :: TransactionInput,
+        output :: TransactionOutput
+    }
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving (ToJSON, FromJSON)
+
+type TransactionUnspentOutputs = [TransactionUnspentOutput]
