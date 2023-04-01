c022cdc5f45b1d3559e44360d3ed55d477ebabdb
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-04-01 11:19:23 +0300

    Add toCSL class

--- src/CSL/Class.hs
+++ src/CSL/Class.hs
@@ -1,27 +1,31 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE NoImplicitPrelude          #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module CSL.Class where
 
-import qualified Data.Map                       as Map
-import           Data.Maybe                     (Maybe)
-import           Data.Text                      (unpack)
-import           Ledger                         (TxOutRef(..), TxId(..), DecoratedTxOut (..), toPubKeyHash, stakingCredential)
-import           Ledger.Ada                     (lovelaceValueOf)
-import           Ledger.Value                   (Value(..), TokenName (..), CurrencySymbol (..))
-import           PlutusTx.AssocMap              hiding (mapMaybe)
-import           PlutusTx.Prelude               hiding (Maybe)
-import           Text.Hex                       (decodeHex)
-import           Text.Read                      (readMaybe)
+import           Cardano.Api.Shelley           (NetworkId)
+import qualified Data.Map                      as Map
+import           Data.Maybe                    (Maybe)
+import           Data.Text                     (pack, unpack)
+import           Ledger                        (Address (Address), DecoratedTxOut (..), TxId (..), TxOutRef (..), noAdaValue,
+                                                stakingCredential, toPubKeyHash)
+import           Ledger.Ada                    (Ada (getLovelace), fromValue, lovelaceValueOf)
+import           Ledger.Value                  (CurrencySymbol (..), TokenName (..), Value (..))
+import           Plutus.V1.Ledger.Api          (Credential (PubKeyCredential))
+import           PlutusAppsExtra.Utils.Address (addressToBech32, bech32ToAddress)
+import           PlutusTx.AssocMap             hiding (mapMaybe)
+import           PlutusTx.Prelude              hiding (Maybe, toList)
+import           Prelude                       (repeat, show)
+import           Text.Hex                      (decodeHex, encodeHex)
+import           Text.Read                     (readMaybe)
 
 import qualified CSL
-import           PlutusAppsExtra.Utils.Address  (bech32ToAddress)
 
 class FromCSL a b where
     fromCSL :: a -> Maybe b
@@ -69,6 +73,47 @@ instance FromCSL CSL.TransactionUnspentOutput (TxOutRef, DecoratedTxOut) where
         ref   <- fromCSL input
         txOut <- fromCSL output
         return (ref, txOut)
-    
+
 instance FromCSL CSL.TransactionUnspentOutputs [(TxOutRef, DecoratedTxOut)] where
     fromCSL = Just . mapMaybe fromCSL
+
+class ToCSL a b where
+    toCSL :: a -> Maybe b
+
+instance ToCSL TxOutRef CSL.TransactionInput where
+    toCSL (TxOutRef (TxId bbs) ind) =
+        Just $ CSL.TransactionInput (encodeHex $ fromBuiltin bbs) ind
+
+instance ToCSL [TxOutRef] CSL.TransactionInputs where
+    toCSL = Just . mapMaybe toCSL
+
+instance ToCSL Value CSL.Value where
+    toCSL val = Just $ CSL.Value (pack $ show $ getLovelace lovelace) (CSL.MultiAsset <$> mma)
+        where
+            lovelace = fromValue val
+            assets   = noAdaValue val
+            mma = if assets == zero then Nothing else Just $ g' $ getValue val
+            g' = Map.fromList . map f' . toList
+            f' (k, m) = let kTxt = encodeHex $ fromBuiltin $ unCurrencySymbol k
+                        in (kTxt, g m)
+            g = Map.fromList . map f . toList
+            f (a, b) = let aTxt = encodeHex $ fromBuiltin $ unTokenName a
+                           bTxt = pack $ show b
+                       in (aTxt, bTxt)
+
+instance ToCSL (DecoratedTxOut, NetworkId) CSL.TransactionOutput where
+    toCSL (PublicKeyDecoratedTxOut pkh scr val _ _, networkId) = do
+        let addr = Address (PubKeyCredential pkh) scr
+        addrCSL <- addressToBech32 networkId addr
+        valCSL  <- toCSL val
+        return $ CSL.TransactionOutput addrCSL valCSL Nothing Nothing
+    toCSL _ = Nothing
+
+instance ToCSL ([DecoratedTxOut], NetworkId) CSL.TransactionOutputs where
+    toCSL (txOuts, network) = Just . mapMaybe toCSL $ zip txOuts $ repeat network
+
+instance ToCSL (TxOutRef, DecoratedTxOut, NetworkId) CSL.TransactionUnspentOutput where
+    toCSL (ref, txOut, network) = CSL.TransactionUnspentOutput <$> toCSL ref <*> toCSL (txOut, network)
+
+instance ToCSL ([(TxOutRef, DecoratedTxOut)], NetworkId) CSL.TransactionUnspentOutputs where
+    toCSL (xs, network) = Just $ mapMaybe (\(ref, txOut) -> toCSL (ref, txOut, network)) xs
